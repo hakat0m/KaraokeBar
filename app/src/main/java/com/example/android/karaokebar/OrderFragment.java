c6a42928faba0d3bd55aa4810ceda87e28ad38d5
Agnieszka Tomkowicz <agnieszka@uigstudio.com>
2017-03-05 13:03:50 +0100

    Add OrderSong list

--- app/src/main/java/com/example/android/karaokebar/OrderFragment.java
+++ app/src/main/java/com/example/android/karaokebar/OrderFragment.java
@@ -3,15 +3,63 @@ package com.example.android.karaokebar;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class OrderFragment extends Fragment {
+
+    private final String image_titles[] = {
+            "Img1",
+            "Img2",
+            "Img3",
+            "Img4",
+            "Img5",
+            "Img6",
+            "Img7",
+            "Img8",
+            "Img9",
+            "Img10",
+            "Img11",
+            "Img12",
+            "Img13",
+    };
+
+    private final Integer image_ids[] = {
+            R.drawable.rihanna3,
+            R.drawable.rihanna3,
+            R.drawable.rihanna3,
+            R.drawable.rihanna3,
+            R.drawable.rihanna3,
+            R.drawable.rihanna3,
+            R.drawable.rihanna3,
+            R.drawable.rihanna3,
+            R.drawable.rihanna3,
+            R.drawable.rihanna3,
+            R.drawable.rihanna3,
+            R.drawable.rihanna3,
+            R.drawable.rihanna3,
+    };
+
+    private ArrayList<SongOrder> prepareData(){
+
+        ArrayList<SongOrder> theimage = new ArrayList<>();
+        for(int i = 0; i< image_titles.length; i++){
+            SongOrder createList = new SongOrder();
+            createList.setImage_title(image_titles[i]);
+            createList.setImage_ID(image_ids[i]);
+            theimage.add(createList);
+        }
+        return theimage;
+    }
 
 
     public OrderFragment() {
@@ -23,7 +71,18 @@ public class OrderFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_order, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_order, container, false);
+
+        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.imagegallery);
+        recyclerView.setHasFixedSize(true);
+
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getActivity().getApplicationContext(),2);
+        recyclerView.setLayoutManager(layoutManager);
+        ArrayList<SongOrder> createLists = prepareData();
+        SongOrderAdapter adapter = new SongOrderAdapter(getActivity().getApplicationContext(), createLists);
+        recyclerView.setAdapter(adapter);
+
+        return rootView;
     }
 
 }
---
+++ app/src/main/java/com/example/android/karaokebar/SongOrderAdapter.java
@@ -0,0 +1,54 @@
+package com.example.android.karaokebar;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.ArrayList;
+
+/**
+ * Created by Aga on 3/5/2017.
+ */
+
+public class SongOrderAdapter extends RecyclerView.Adapter<SongOrderAdapter.ViewHolder> {
+    private ArrayList<SongOrder> galleryList;
+    private Context context;
+
+    public SongOrderAdapter(Context context, ArrayList<SongOrder> galleryList) {
+        this.galleryList = galleryList;
+        this.context = context;
+    }
+
+    @Override
+    public SongOrderAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.cell_layout, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(SongOrderAdapter.ViewHolder viewHolder, int i) {
+        viewHolder.title.setText(galleryList.get(i).getImage_title());
+        viewHolder.img.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        viewHolder.img.setImageResource((galleryList.get(i).getImage_ID()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return galleryList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        private TextView title;
+        private ImageView img;
+        public ViewHolder(View view) {
+            super(view);
+
+            title = (TextView)view.findViewById(R.id.title);
+            img = (ImageView) view.findViewById(R.id.img);
+        }
+    }
+}
+
