b4f0d4f2177985cc6fddc10703adace13fbf17fc
Agnieszka Tomkowicz <agnieszka@uigstudio.com>
2017-03-05 21:33:32 +0100

    Add images to OrderSong list

--- app/src/main/java/com/example/android/karaokebar/SongListAdapter.java
+++ app/src/main/java/com/example/android/karaokebar/SongListAdapter.java
@@ -42,7 +42,7 @@ public class SongListAdapter extends ArrayAdapter<Song> {
                 fab.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        Snackbar.make(view, "Thank you for voting", Snackbar.LENGTH_LONG)
                                 .setAction("Action", null).show();
                     }
                 });
